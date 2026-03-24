237abb61f3f19f18cd763d6733154a92bde39f8b
Bardy <bardachev2@mail.ru>
2026-03-24 17:33:54 +0300

    init matchers project

---
+++ src/js/__tests__/sortHeroes.test.js
@@ -0,0 +1,17 @@
+const sortHeroes = require('../sortHeroes');
+
+test('should sort heroes by health descending', () => {
+  const heroes = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+
+  const result = sortHeroes(heroes);
+
+  expect(result).toEqual([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
+});
